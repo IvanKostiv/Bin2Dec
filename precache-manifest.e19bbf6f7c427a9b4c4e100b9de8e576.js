d46a515832130bcc68c3618ad57d3cda5ef0e4f7
Ivan Kostiv <ivankostiv.pz@gmail.com>
2020-09-10 21:12:05 +0300

    Updates

---
+++ precache-manifest.e19bbf6f7c427a9b4c4e100b9de8e576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc2fbaeeace78129c3b6fec9a5130d6",
+    "url": "/Bin2Dec/index.html"
+  },
+  {
+    "revision": "cffeb774cc6a90ad4b6c",
+    "url": "/Bin2Dec/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "8d21dedc6eed5a9dd312",
+    "url": "/Bin2Dec/static/css/main.c80094ea.chunk.css"
+  },
+  {
+    "revision": "cffeb774cc6a90ad4b6c",
+    "url": "/Bin2Dec/static/js/2.739abf59.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Bin2Dec/static/js/2.739abf59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d21dedc6eed5a9dd312",
+    "url": "/Bin2Dec/static/js/main.ce0c42c2.chunk.js"
+  },
+  {
+    "revision": "20abba65e287c6c675d5",
+    "url": "/Bin2Dec/static/js/runtime-main.3c697365.js"
+  }
+]);
